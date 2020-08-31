95703c8533997e2456e25c3b21682e1245e5f3e7
Kunal-Moz <kunalmoz@buffalo.edu>
2020-08-31 09:37:23 -0400

    Discontinuous model

---
+++ NoneqTrans/ccr_codes/py_mpi_run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --time=30:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+##SBATCH --cpus-per-task=1
+#SBATCH --mem=16000
+##SBATCH --clusters=mae
+#SBATCH --mail-user=kunalmoz@buffalo.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name='python_rst_run2'
+#SBATCH --output=rs_glt-%j.dat
+#SBATCH --error=rs_glt-%j.err
+#SBATCH --partition=general-compute --qos=general-compute
+##SBATCH --partition=debug --qos=debug
+#SBATCH --requeue
+#SBATCH --exclusive
+
+# load modules
+module load python/anaconda
+module load intel/17.0
+module load mkl/2018.3
+module load gcc/7.3.0
+module load intel-mpi
+
+
+ulimit -s unlimited
+
+# enable mpi4py module
+export PYTHONPATH=/util/academic/python/mpi4py/v2.0.0/lib/python2.7/site-packages:$PYTHONPATH
+
+# launch app
+export I_MPI_FABRICS_LIST=tcp
+export I_MPI_DEBUG=4
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
+
+srun -n $SLURM_NPROCS python glt_mpi.py
+
+
+##echo "Okay!! Run python 3 for 64x64"
+
+
